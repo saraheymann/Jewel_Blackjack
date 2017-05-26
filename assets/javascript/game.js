9c652ee72fccc8db1dd243c35ff274b6bc532751
sara heymann <saraheymann@saras-mbp.attlocal.net>
2017-05-26 15:18:15 -0500

    updated css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,14 +6,14 @@ var userTotalScore = 0;
 // generates a random number for the computer
 var valueNumber = Math.floor(Math.random() * 102)+ 19;
 
+// shows the computer's number to match in value box
+$("#valueBox").html(valueNumber);
+
 // generates a random number for the crystals
 var crystalOne = Math.floor(Math.random() * 12)+ 1;
 var crystalTwo = Math.floor(Math.random() * 12)+ 1;
 var crystalThree = Math.floor(Math.random() * 12)+ 1;
 var crystalFour = Math.floor(Math.random() * 12)+ 1;
-
-// shows the computer's number to match in value box
-$("#valueBox").html(valueNumber);
 
 // assign the crystal numbers to their buttons
 $("#crystalOneId").html(crystalOne);
@@ -90,21 +90,4 @@ $("#crystalFourId").click(function addCrystalFour(){
 };
 
 
-
-
-
-// 	// when you click the button it adds to the score
-// 	// if the score of the buttons equals the score of valueBox then you win
-// 	// add a win
-// 	// reset
-// 	// if the score of the buttons goes over the score of valueBox then you lose
-// 	// add a loss
-// 	// reset
-// });
-// $('#wins')
-// 	// put wins here
-// $('losses')
-// 	// put losses here
-// $('.totalScore')
-// 	// total score goes here
 // };
