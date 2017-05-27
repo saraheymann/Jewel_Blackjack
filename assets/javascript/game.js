795915d8fa6519909095bb023beb948012974493
sara heymann <saraheymann@saras-mbp.attlocal.net>
2017-05-26 22:09:29 -0500

    still working on reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-(window).onload = function(){
+$(document).ready(function(){
 var wins= 0;
 var losses = 0;
 var userTotalScore = 0;
@@ -7,7 +7,7 @@ var userTotalScore = 0;
 var valueNumber = Math.floor(Math.random() * 102)+ 19;
 
 // shows the computer's number to match in value box
-$("#valueBox").html(valueNumber);
+$("#valueBox").text(valueNumber);
 
 // generates a random number for the crystals
 var crystalOne = Math.floor(Math.random() * 12)+ 1;
@@ -16,78 +16,80 @@ var crystalThree = Math.floor(Math.random() * 12)+ 1;
 var crystalFour = Math.floor(Math.random() * 12)+ 1;
 
 // assign the crystal numbers to their buttons
-$("#crystalOneId").html(crystalOne);
-$("#crystalTwoId").html(crystalTwo);
-$("#crystalThreeId").html(crystalThree);
-$("#crystalFourId").html(crystalFour);
+// $("#crystalOneId").html(crystalOne);
+// $("#crystalTwoId").html(crystalTwo);
+// $("#crystalThreeId").html(crystalThree);
+// $("#crystalFourId").html(crystalFour);
+
+function winGame(){
+	wins++;
+	$("#wins").text('Wins: '+ wins);
+	reset();
+}
+function loseGame(){
+	losses++;
+	$("#losses").text('Losses: '+ losses);
+	reset();
+}
 
 // reset
 function reset(){
-	var userTotalScore = 0;
 	var valueNumber = Math.floor(Math.random() * 102)+ 19;
 	var crystalOne = Math.floor(Math.random() * 12)+ 1;
 	var crystalTwo = Math.floor(Math.random() * 12)+ 1;
 	var crystalThree = Math.floor(Math.random() * 12)+ 1;
 	var crystalFour = Math.floor(Math.random() * 12)+ 1;
+	var userTotalScore = 0;
+	$(".totalScore").text(userTotalScore);
+	$("#valueBox").text(valueNumber);
 }
 
 // when the crystal buttons are clicked, add them to total score
-
 $("#crystalOneId").click(function addCrystalOne(){
 	userTotalScore = userTotalScore + crystalOne;
 	$(".totalScore").text(userTotalScore);
+	
 	if(userTotalScore === valueNumber){
-	reset();
-	wins++;
-	$("#wins").text('Wins: '+ wins);
-	
-}else if(userTotalScore > valueNumber){
-	reset();
-	losses++;
-	$("#losses").text('Losses: '+ losses);
-	
+	winGame();
+
+	}else if(userTotalScore > valueNumber){
+	loseGame();
 }
 })
 $("#crystalTwoId").click(function addCrystalTwo(){
 	 userTotalScore = userTotalScore + crystalTwo;
 	$(".totalScore").text(userTotalScore);
+	
 	if(userTotalScore === valueNumber){
-	reset();
-	wins++;
-	$("#wins").text('Wins: '+ wins);
-}else if (userTotalScore > valueNumber){
-	reset();
-	losses++;
-	$("#losses").text('Losses: '+ losses);
+	winGame();
+
+	}else if (userTotalScore > valueNumber){
+	loseGame();
 }
 })
 $("#crystalThreeId").click(function addCrystalThree(){
 	 userTotalScore = userTotalScore + crystalTwo;
 	$(".totalScore").text(userTotalScore);
+	
 	if(userTotalScore === valueNumber){
-	wins++;
-	$("#wins").text('Wins: '+ wins);
-	reset();
-}else if (userTotalScore > valueNumber){
-	losses++;
-	$("#losses").text('Losses: '+ losses);
-	reset();
+	winGame();
+	
+	}else if (userTotalScore > valueNumber){
+	loseGame();
 }
 })
 $("#crystalFourId").click(function addCrystalFour(){
 	 userTotalScore = userTotalScore + crystalTwo;
 	$(".totalScore").text(userTotalScore);
+	
 	if(userTotalScore === valueNumber){
-	wins++;
-	$("#wins").text('Wins: '+ wins);
-	reset();
-}else if (userTotalScore > valueNumber){
-	losses++;
-	$("#losses").text('Losses: '+ losses);
-	reset();
+	winGame();
+	
+	}else if (userTotalScore > valueNumber){
+	loseGame();
 }
 })
-};
+});
 
 
 // };
